e39e3d226ea2be84376ed7414af677295763b53e
Gregory Losco <gtlosco@gmail.com>
2019-03-28 00:51:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,20 @@
 def oxford_comma(number)
   if number.size == 1 
-    number.join
+      number.join
     elsif number.size == 2 
-    number.join(" and ")
+      number.join(" and ")
     elsif number.size >= 3
-      while 
+       counter = 0 
+       new_array = []
+       number.each {|word| 
+       if counter < number.length - 1 
+       new_array << word
+       counter += 1 
+      else
+        new_array << "and #{word}"
+      end
     #number[-1] = "and #{number[-1]}"
-   # number.join(", ")
+    #number.join(", ")
+    new_array.join(", ")
   end
 end
